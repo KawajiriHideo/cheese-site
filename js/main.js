53bf0474174ecdc21e0db00d16e768ff34adcb5c
KawajiriHideo <hide.kawa.0001@gmail.com>
2020-03-24 19:51:21 +0900

    renew

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // jsを記述する際はここに記載していく
+jQuery(function () {
+    var pagetop = $('#page_top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    $('a[href^="#"]').click(function () {
+        var time = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" ? 'html' : href);
+        var distance = target.offset().top;
+        $("html, body").animate({ scrollTop: distance }, time, "swing");
+        return false;
+    });
+});
+
+function clickEvent() {
+    alert('※チーズアカデミーは実際には存在しません。');
+}
